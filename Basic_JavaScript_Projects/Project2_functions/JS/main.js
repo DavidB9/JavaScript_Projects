37cc091350edc2630f86528324c79434de38236d
David Buckingham <davidbuckingham95@gmail.com>
2021-06-22 14:07:42 +0100

    Committing Assignment 2
    Adding Project_functions folder

---
+++ Basic_JavaScript_Projects/Project2_functions/JS/main.js
@@ -0,0 +1,12 @@
+function testFunction(){
+    var num1 = 5;
+    var num2 = 10;
+    var result = num1 + num2;
+    document.getElementById("test").innerHTML= result;
+}
+
+function myFunction(){
+    var sentence= "I am learning ";
+    sentence+="a lot from this course";
+    document.getElementById("concat").innerHTML = sentence;
+}
