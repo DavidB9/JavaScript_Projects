1210b121a73352c593c71a43b9f0ed554ef3893e
David Buckingham <davidbuckingham95@gmail.com>
2021-06-22 13:49:11 +0100

    Committing Assignment 1
    Adding Basic_JavaScript_Projects folder

---
+++ Basic_JavaScript_Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,16 @@
+var test = "This is a test string"; //create a varible with a string
+var alertMessage = "Hello, World!"; //create a varaible with a string for the alert message
+var testString = "This is a test \"quote\" "; //create a variable with a string to test \
+
+var family = "Buckinghams", Dad="Alan", Mom="Pauline",daughter="Amanda",son="David"; //create a group of varibles assign them strings
+
+//change the color of the strings
+Dad = Dad.fontcolor("blue");
+Mom = Mom.fontcolor("red");
+daughter = daughter.fontcolor("green");
+son = son.fontcolor("yellow");
+
+
+window.alert(alertMessage); //Display an alert message with the alertMessage variable
+document.write(Dad + Mom + daughter + son); //Display concated strings
+document.write(10 + 10); //Display experession of 10 + 10
